72d6ce4430c1650c9b970790a91f27aafbac20da
xsf0105 <allan91coder@gmail.com>
2023-04-12 15:18:04 +0800

    feat: update

--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -2,13 +2,14 @@ import { QuarkElement, Fragment, customElement } from "quarkc";
 import style from "./index.css?inline";
 import md from "./defining.md"
 
-import showdown from "showdown";
+// import showdown from "showdown";
 
-const converter = new showdown.Converter()
-const text = '# hello, markdown!';
-const html = converter.makeHtml(text);
+// const converter = new showdown.Converter()
 
-console.log(text, html, 11);
+// const text = '# hello, markdown!';
+// const html = converter.makeHtml(text);
+
+// console.log(text, html, 11);
 console.log(md, 222);
 
 
