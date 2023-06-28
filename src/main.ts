ec5679774920b29fd2c6713e30ca0746dbd9af0e
xsf0105 <allan91coder@gmail.com>
2023-06-28 17:14:47 +0800

    release: update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { Router } from '@vaadin/router'
+import "./main.less"
+
+import './views/NotFound'
 
 const outlet = document.querySelector('#root');
 export const router = new Router(outlet);
@@ -8,8 +11,28 @@ router.setRoutes([{
     component: 'app-home', // custom element name
     action: async () => { await import('./views/home/index'); }
   }, {
-    path: '/docs',
-    component: 'app-docs',
-    action: async () => { await import('./views/docs'); }
-  },
-]);
+    path: '/sub',
+    component: 'app-sub',
+    action: async () => { await import('./views/sub'); }
+  }, {
+    path: '(.*)',
+    component: 'app-not-found'
+  }
+])
+
+// router.setRoutes([
+//   { path: '/', component: 'hello-home' },
+//   { path: '/a', component: 'hello-a' },
+//   { path: '/b', component: 'hello-b' },
+//   {
+//     path: '/nav',
+//     component: 'hello-nav',
+//     children: [
+//       { path: '/a', component: 'hello-a' },
+//       { path: '/b', component: 'hello-b' },
+//       { path: '(.*)', component: 'hello-not-found' },
+//     ],
+//   },
+//   { path: '/fruit/:id', component: 'hello-fruit' },
+//   { path: '(.*)', component: 'hello-not-found' },
+// ]);
